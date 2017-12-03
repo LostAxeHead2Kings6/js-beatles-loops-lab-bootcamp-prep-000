8d4c783e0a42c737083a7c2c8f446949ab273389
Michael Vasios <mvasios1@gmail.com>
2017-12-03 20:05:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(arr1, arr2) {
   for (var i = 0; i < arr1.length; i++) {
-    console.log("${arr1[0]} plays ${arr2[0]}")
+    console.log("${arr1[i]} plays ${arr2[i]}")
   }
 }

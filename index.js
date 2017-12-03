a67648a94e86770508d77d190fa6fcef773fd3a0
Michael Vasios <mvasios1@gmail.com>
2017-12-03 20:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function theBeatlesPlay(arr1, arr2) {
 
 function johnLennonFacts(arr) {
   var i = 0;
-  while (i < arr.length)
+  while (i < arr.length) {
+    arr[i] = arr[i] + "!!!";
+    i = i + 1;
+  }
 }
